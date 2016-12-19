b84f28417bd956649128accfc9f78cca0bdc12f7
freeman286 <molemayer@gmail.com>
2016-12-19 16:29:29 +0100

    Initial player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public PlayerMotor motor;
+    public Camera cam;
+
+    private float xRot;
+    private float yRot;
+    private float zRot;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        xRot = -Input.GetAxis("Mouse Y") * Time.deltaTime;
+        yRot = Input.GetAxis("Mouse X") * Time.deltaTime;
+        zRot = Input.GetAxis("Horizontal") * Time.deltaTime;
+        motor.Rotate(xRot, yRot, zRot);
+
+        Vector3 moveCamTo = transform.position - transform.forward * motor.speed * 0.2f + transform.up * 5f;
+        cam.transform.position = moveCamTo;
+        cam.transform.LookAt(transform.position + transform.forward * 30f);
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : MonoBehaviour {
+    public Rigidbody rb;
+
+    public float speed;
+    public float _xSen;
+    public float _ySen;
+    public float _zSen;
+
+    public void Rotate (float _x, float _y, float _z) {
+        transform.Rotate(Mathf.Clamp(_x * _xSen, -1f, 1f), Mathf.Clamp(_y * _ySen, -1, 1f), Mathf.Clamp(_z * _zSen, -1f, 1f));
+    }
+
+    void Update () {
+        transform.position += transform.forward * speed * Time.deltaTime;
+        speed -= transform.forward.y * 50f * Time.deltaTime;
+
+        speed = Mathf.Clamp(speed, 35, 200);
+    }
+}
