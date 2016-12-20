3cfc332da81506e3f9f5064e9257a16474e84561
freeman286 <molemayer@gmail.com>
2016-12-20 11:19:08 +0100

    Add movement syncing to players

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    public Behaviour[] ComponentsToDisable;
+
+    Camera sceneCamera;
+
+    public string remoteLayerName;
+
+    public GameObject playerGraphics;
+
+    void Start() {
+        if (!isLocalPlayer) {
+            DisableComponents();
+            AssignRemoteLayer();
+        } else {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null) {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void SetLayerRecursively(GameObject obj, int newLayer) {
+        obj.layer = newLayer;
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, newLayer);
+        }
+    }
+
+    void DisableComponents() {
+        for (int i = 0; i < ComponentsToDisable.Length; i++){
+            ComponentsToDisable[i].enabled = false;
+        }
+    }
+
+    void AssignRemoteLayer() {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    void OnDisable() {
+        if (sceneCamera != null){
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
